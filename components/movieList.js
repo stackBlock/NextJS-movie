5e75698babab8aaad5ec00058afa4cb78d703e24
stackblock <stackchain@gmail.com>
2020-05-28 21:43:29 -0400

    Iterating through the movies and controlling the ratings corectly.

--- components/movieList.js
+++ components/movieList.js
@@ -2,177 +2,53 @@ import React from "react";
 
 class MovieList extends React.Component {
   render() {
-    const { count } = this.props;
+    const { movies } = this.props;
     return (
       <React.Fragment>
-        <div>
-          <h1>{count}</h1>
-        </div>
-
-        <div className="col-lg-4 col-md-6 mb-4">
-          <div className="card h-100">
-            <a href="#">
-              <img
-                className="card-img-top"
-                src="http://placehold.it/700x400"
-                alt=""
-              />
-            </a>
-            <div className="card-body">
-              <h4 className="card-title">
-                <a href="#">Item One</a>
-              </h4>
-              <h5>$24.99</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-                numquam aspernatur!
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">
-                &#9733; &#9733; &#9733; &#9733; &#9734;
-              </small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-md-6 mb-4">
-          <div className="card h-100">
-            <a href="#">
-              <img
-                className="card-img-top"
-                src="http://placehold.it/700x400"
-                alt=""
-              />
-            </a>
-            <div className="card-body">
-              <h4 className="card-title">
-                <a href="#">Item One</a>
-              </h4>
-              <h5>$24.99</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-                numquam aspernatur!
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">
-                &#9733; &#9733; &#9733; &#9733; &#9734;
-              </small>
+        {movies.map((movie) => {
+          return (
+            <div className="col-lg-4 col-md-6 mb-4">
+            <div className="card h-100">
+              <a href="#">
+                <img
+                  className="card-img-top"
+                  src={movie.cover}
+                  alt=""
+                />
+              </a>
+              <div className="card-body">
+                <h4 className="card-title">
+                  <a href="#">{movie.name}</a>
+                </h4>
+                  <h5>$24.99</h5>
+                <p className="card-text">
+                  {movie.description}
+                </p>
+              </div>
+              <div className="card-footer">
+              {parseInt(movie.rating) >= 5 ? (<small className="text-muted">
+                  &#9733; &#9733; &#9733; &#9733; &#9733;
+                </small>) : parseInt(movie.rating) >= 4 ? (<small className="text-muted">
+                  &#9733; &#9733; &#9733; &#9733; &#9734;
+                </small>) : parseInt(movie.rating) >= 3 ? (<small className="text-muted">
+                  &#9733; &#9733; &#9733; &#9734; &#9734;
+                </small>) : parseInt(movie.rating) >= 2 ? (<small className="text-muted">
+                  &#9733; &#9733; &#9734; &#9734; &#9734;
+                </small>) : parseInt(movie.rating) >= 1 ? (<small className="text-muted">
+                  &#9733; &#9734; &#9734; &#9734; &#9734;
+                </small>) : (<small className="text-muted">
+                  &#9734; &#9734; &#9734; &#9734; &#9734;
+                </small>) }
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="col-lg-4 col-md-6 mb-4">
-          <div className="card h-100">
-            <a href="#">
-              <img
-                className="card-img-top"
-                src="http://placehold.it/700x400"
-                alt=""
-              />
-            </a>
-            <div className="card-body">
-              <h4 className="card-title">
-                <a href="#">Item One</a>
-              </h4>
-              <h5>$24.99</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-                numquam aspernatur!
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">
-                &#9733; &#9733; &#9733; &#9733; &#9734;
-              </small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-md-6 mb-4">
-          <div className="card h-100">
-            <a href="#">
-              <img
-                className="card-img-top"
-                src="http://placehold.it/700x400"
-                alt=""
-              />
-            </a>
-            <div className="card-body">
-              <h4 className="card-title">
-                <a href="#">Item One</a>
-              </h4>
-              <h5>$24.99</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-                numquam aspernatur!
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">
-                &#9733; &#9733; &#9733; &#9733; &#9734;
-              </small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-md-6 mb-4">
-          <div className="card h-100">
-            <a href="#">
-              <img
-                className="card-img-top"
-                src="http://placehold.it/700x400"
-                alt=""
-              />
-            </a>
-            <div className="card-body">
-              <h4 className="card-title">
-                <a href="#">Item One</a>
-              </h4>
-              <h5>$24.99</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-                numquam aspernatur!
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">
-                &#9733; &#9733; &#9733; &#9733; &#9734;
-              </small>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-md-6 mb-4">
-          <div className="card h-100">
-            <a href="#">
-              <img
-                className="card-img-top"
-                src="http://placehold.it/700x400"
-                alt=""
-              />
-            </a>
-            <div className="card-body">
-              <h4 className="card-title">
-                <a href="#">Item One</a>
-              </h4>
-              <h5>$24.99</h5>
-              <p className="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
-                numquam aspernatur!
-              </p>
-            </div>
-            <div className="card-footer">
-              <small className="text-muted">
-                &#9733; &#9733; &#9733; &#9733; &#9734;
-              </small>
-            </div>
-          </div>
-        </div>
+          )
+        })}
       </React.Fragment>
     );
   }
 }
 
 export default MovieList;
+
+// {movies.rating >= 4 ? rating : rating}
--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
 
-const SideMenu = ({ count, appName, clickHandler }) => {
+const SideMenu = ({ appName }) => {
   return (
     <div>
-      <button onClick={clickHandler}>Click ME!!!</button>
       <h1 className="my-4">{appName}</h1>
       <div className="list-group">
         <a href="#" className="list-group-item">
@@ -15,9 +14,6 @@ const SideMenu = ({ count, appName, clickHandler }) => {
         <a href="#" className="list-group-item">
           Category 3
         </a>
-      </div>
-      <div>
-        <h1>{count}</h1>
       </div>
     </div>
   );
