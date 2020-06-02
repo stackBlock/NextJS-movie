0208cd0642d11d063b3dc2ee51abbfad8354fddd
stackblock <stackchain@gmail.com>
2020-06-02 18:48:06 -0400

    P 41 setting up backend

--- server/index.js
+++ server/index.js
@@ -1,23 +1,30 @@
-const next = require('next')
-const express = require('express');
+const next = require("next");
+const express = require("express");
+const bodyParser = require('body-parser');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-
   const server = express();
-  // We are handling all of the requests coming to our server
-  server.get('*', (req, res) => {
+  server.use(bodyParser.json())
+
+  server.get('/api/v1/movies', (req, res) => {
+    return res.json({ message: "Hello World" });
+  });
+
+  
+   // We are handling all of the requests coming to our server
+  server.get("*", (req, res) => {
     // next.js is handling requests and providing pages where we are navigating to
-    return handle(req, res)
-  })
+    return handle(req, res);
+  });
 
   const PORT = process.env.PORT || 3000;
 
-  server.use(handle).listen(PORT, (err) => {
-    if (err) throw err
-    console.log('> Ready on port ' + PORT)
-  })
-})
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log("> Ready on port " + PORT);
+  });
+});
