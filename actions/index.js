5bbccff6172122a881fd3c8e7b808cc6523a377f
stackblock <stackchain@gmail.com>
2020-06-03 13:32:05 -0400

    functionality done, working on some minor tweeks

--- actions/index.js
+++ actions/index.js
@@ -387,6 +387,12 @@ export const getMovieById = (id) => {
   return axios.get(`${BASE_URL}/api/v1/movies/${id}`).then((res) => res.data);
 };
 
+export const updateMovie = (movie) => {
+  return axios
+    .patch(`${BASE_URL}/api/v1/movies/${movie.id}`, movie)
+    .then((res) => res.data);
+};
+
 export const createMovie = (movie) => {
   movie.id = Math.random().toString(36).substr(2, 7);
   console.log(movie);
@@ -394,5 +400,7 @@ export const createMovie = (movie) => {
 };
 
 export const deleteMovie = (id) => {
-  return axios.delete(`${BASE_URL}/api/v1/movies/${id}`).then((res) => res.data);
+  return axios
+    .delete(`${BASE_URL}/api/v1/movies/${id}`)
+    .then((res) => res.data);
 };
